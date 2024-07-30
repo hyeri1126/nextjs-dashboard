600b3c18714d1a8c1c02fb0ac08628b0f7ed2bde
hyeri1126 <hyeri1126@ewhain.net>
2024-07-30 15:24:28 +0900

    nextjs practice5까지 완료

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,12 @@
-export default function RootLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+import "@/app/ui/global.css"
+
+// #2-1-2. 폰트 불러오기(import)
+import {inter} from './ui/fonts'
+
+export default function RootLayout({children,}: {children: React.ReactNode;}) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={inter.className} >{children}</body>
     </html>
   );
 }
---
+++ app/ui/fonts.ts
@@ -0,0 +1,5 @@
+import {Inter, Lusitana} from "next/font/google"
+
+// 외부에서 import하여 사용할 수 있도록 작성
+export const inter = Inter({subsets:['latin']});
+export const lusitana = Lusitana({subsets:['latin'], weight:['400']})
